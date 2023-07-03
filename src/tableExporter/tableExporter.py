c378c30451205240b1185ed82cdd4b046c629f5b
Sascha Resch (mobil) <resch.sjj@gmail.com>
2023-07-03 16:28:42 +0200

    First version TableExporter

--- src/tableExporter/tableExporter.py
+++ src/tableExporter/tableExporter.py
@@ -1,6 +1,21 @@
+import csv
+
 class TableExporter:
-    def __init__(self, data):
+    def __init__(self, lableData, data, outputFile):
         self.data = data
+        self.lableData = lableData
+        self.outputFile = outputFile
 
     def export(self):
+        with open(self.outputFile, 'w', newline='') as file:
+            # Manage CSV writer settings
+            csv.register_dialect("custom", delimiter=";", skipinitialspace=True)
+            writer = csv.writer(file, dialect="custom")
+
+             # Write headline
+            writer.writerow(self.lableData)
+
+            for x in self.data:
+                writer.writerow(x)
+
         return "TableExporter.export() works"
--- src/test/testTableExporter.py
+++ src/test/testTableExporter.py
@@ -2,5 +2,5 @@ from tableExporter import TableExporter
 
 
 def testTableExporter():
-    tableExporterObject = TableExporter([])
+    tableExporterObject = TableExporter(["A", "E", "I", "O", "U"], [[10, 10, 20, 14, 7]], "export/test.csv")
     print(tableExporterObject.export())
