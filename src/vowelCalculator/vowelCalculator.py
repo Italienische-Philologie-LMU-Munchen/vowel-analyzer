529922f960d42ddc83f427fc1bb5e06311b1eabd
enricofa <enrico.fantini@sns.it>
2023-07-21 11:50:44 +0200

    Switch to regular expressions

--- src/vowelCalculator/vowelCalculator.py
+++ src/vowelCalculator/vowelCalculator.py
@@ -1,3 +1,4 @@
+import re
 class VowelCalculator:
     def __init__(self, text):
         self.text = text
@@ -11,31 +12,14 @@ class VowelCalculator:
         uCounter = 0
         for x in self.text:
             if isinstance(x, str):
-                aCounter = aCounter + x.count("a")
-                aCounter = aCounter + x.count("à")
-                aCounter = aCounter + x.count("á")
-                aCounter = aCounter + x.count("â")
-                aCounter = aCounter + x.count("ä")
-                eCounter = eCounter + x.count("e")
-                eCounter = eCounter + x.count("è")
-                eCounter = eCounter + x.count("é")
-                eCounter = eCounter + x.count("ë")
-                eCounter = eCounter + x.count("ê")
-                iCounter = iCounter + x.count("i")
-                iCounter = iCounter + x.count("ì")
-                iCounter = iCounter + x.count("í")
-                iCounter = iCounter + x.count("ï")
-                iCounter = iCounter + x.count("î")
-                oCounter = oCounter + x.count("o")
-                oCounter = oCounter + x.count("ò")
-                oCounter = oCounter + x.count("ó")
-                oCounter = oCounter + x.count("ô")
-                oCounter = oCounter + x.count("ö")
-                uCounter = uCounter + x.count("u")
-                uCounter = uCounter + x.count("ù")
-                uCounter = uCounter + x.count("ú")
-                uCounter = uCounter + x.count("û")
-                uCounter = uCounter + x.count("ü")
+                x = x.lower()
+                aCounter += len(re.findall("[aàáäâ]", x))
+                eCounter += len(re.findall("[eéèêë]", x))
+                iCounter += len(re.findall("[iìíîï]", x))
+                oCounter += len(re.findall("[oòóôö]", x))
+                uCounter += len(re.findall("[uùúûü]", x))
+               
+               
 
         return [["A", "E", "I", "O", "U"], [aCounter, eCounter, iCounter, oCounter, uCounter]]
            
