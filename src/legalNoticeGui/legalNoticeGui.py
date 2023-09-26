a35000a38ee6757529605ea9812dcce52e46ada1
Sascha Resch <s.resch@42-projects.com>
2023-09-26 10:59:35 +0200

    Add legal notice window and minor GUI changes (icons)

---
+++ src/legalNoticeGui/legalNoticeGui.py
@@ -0,0 +1,70 @@
+import os
+from pathlib import Path
+from PyQt5.QtWidgets import QMainWindow, QLabel, QTextEdit
+from PyQt5.QtGui import QIcon
+
+
+class LegalNoticeGui(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # Save current directory, so we can access it for icons
+        self.current_directory = os.path.dirname(__file__)
+
+        # Title of application window and icon
+        self.setWindowTitle("Vowel Analyzer - Legal Notice")
+
+        # Icon handling
+        self.window_icon = str(Path(self.current_directory).parent.absolute() /
+                               'assets' / 'scale-balanced-solid.svg')
+        self.setWindowIcon(QIcon(self.window_icon))
+
+        # Fixed geometry for startup of application
+        self.setGeometry(0, 0, 400, 400)
+        self.setMinimumSize(400, 400)
+
+        # Heading for legal text to be displayed
+        self.legalTextHeading = 'Legal Notice'
+        self.lbLegalTextHeading = QLabel(self.legalTextHeading, self)
+
+        # Main legal text to be displayed
+        self.legalText = '''Vowel Analyzer
+Version: 0.1.0.0 
+
+Copyright: ©2023, GNU GPLv3 License 
+
+Enrico Fantini, LMU Munich and Sascha Resch, LMU Munich'''
+
+        self.lbLegalText = QLabel(self.legalText, self)
+
+        # List of libraries and copyright mentions to be displayed (read only mode!)
+        self.legalTextLibraries = '''Software to analyze vowel phonetics in (Old) Italian texts 
+        <br/><br/>
+        The following resources have been used: 
+        <br/><br/>
+        PyQt5: https://www.riverbankcomputing.com/static/Docs/PyQt5/, GPL v3-License 
+        <br/><br/>
+        Pygal: https://github.com/Kozea/pygal,  LGPL 3.0-License
+        <br/><br/>
+        lxml: https://github.com/lxml/lxml, BSD License: Copyright (c) 2004 Infrae. All rights reserved.
+        <br/><br/>
+        PyInstaller: https://github.com/pyinstaller/pyinstaller, GNU GPL-License
+        <br/><br/>
+        python-dotenv: https://github.com/theskumar/python-dotenv, BSD License: Copyright (c) 2014, Saurabh Kumar (python-dotenv), 2013, Ted Tieken (django-dotenv-rw), 2013, Jacob Kaplan-Moss (django-dotenv)
+        <br/><br/>
+        Free Fontawesome icons: https://fontawesome.com/,  SIL OFL 1.1 license'''
+
+        self.lbLegalTextLibraries = QTextEdit(self.legalTextLibraries, self)
+        self.lbLegalTextLibraries.setReadOnly(True)
+
+        # Set geometry of widgets
+        self.lbLegalTextHeading.setGeometry(10, 10, 300, 40)
+        self.lbLegalText.setGeometry(10, 55, 350, 100)
+        self.lbLegalTextLibraries.setGeometry(
+            10, 165, self.width() - 50, self.height() - 175)
+
+    # Method to handle resizing of window, so widgets aren't distorted
+    def resizeEvent(self, event):
+        self.lbLegalTextLibraries.setGeometry(
+            10, 165, self.width() - 50, self.height() - 175)
+        QMainWindow.resizeEvent(self, event)
--- src/vowelAnalyzerGui.py
+++ src/vowelAnalyzerGui.py
@@ -1,24 +1,43 @@
 import os
 import tempfile
+import platform
+import ctypes
 from shutil import copyfile
 from pathlib import Path
 from PyQt5.QtCore import QUrl
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog, QCheckBox
 from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtGui import QIcon
 from teiParser import TeiParser
 from vowelCalculator import VowelCalculator
 from chartExporter import ChartExporter
+from legalNoticeGui import LegalNoticeGui
 
 
 class VowelAnalyzerGui(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        # Variable to check whether we have already run analysis
+        self.analysisRun = False
+
+        # Icon handling
+        self.current_directory = os.path.dirname(__file__)
+        self.window_icon = str(Path(self.current_directory) /
+                               'assets' / 'magnifying-glass-chart-solid.svg')
+        self.legal_icon = str(Path(self.current_directory) /
+                              'assets' / 'scale-balanced-solid.svg')
+        self.setWindowIcon(QIcon(self.window_icon))
+
         # Title of application window
         self.setWindowTitle("Vowel Analyzer")
 
         # Fixed geometry for startup of application
         self.setGeometry(0, 0, 600, 600)
+        self.setMinimumSize(600, 600)
+
+        # Subwindow for legal notice
+        self.legalWindow = LegalNoticeGui()
 
         # Select file button
         self.btFileSelect = QPushButton("Select file", self)
@@ -31,12 +50,18 @@ class VowelAnalyzerGui(QMainWindow):
         self.btOutputDir.clicked.connect(self.export)
         # Percentage toggle
         self.cbPercentage = QCheckBox("Calculate\r\npercentages", self)
+        self.cbPercentage.stateChanged.connect(self.changePercentView)
+        # Legal notice
+        self.btLegalNotice = QPushButton("Legal Notice", self)
+        self.btLegalNotice.clicked.connect(self.legal)
+        self.btLegalNotice.setIcon(QIcon(self.legal_icon))
 
         # Geometry of buttons
         self.btFileSelect.setGeometry(10, 10, 100, 40)
         self.btAnalysis.setGeometry(10, 55, 100, 40)
         self.btOutputDir.setGeometry(10, 100, 100, 40)
         self.cbPercentage.setGeometry(10, 145, 100, 40)
+        self.btLegalNotice.setGeometry(10, 190, 100, 40)
 
         # Webview to display result SVG
         self.wvResult = QWebEngineView(self)
@@ -52,28 +77,40 @@ class VowelAnalyzerGui(QMainWindow):
         self.temppath = Path(tempfile.gettempdir()) / "VowelAnalyzer"
         self.temppath.mkdir(parents=True, exist_ok=True)
 
+    # Method to select file to be analyzed via dialog box
     def selectFile(self):
+        # Set base directory as default
         baseDirectory = str(Path.home() / 'Desktop')
+        # Get filepath from dialog
         self.filepath, filter = QFileDialog.getOpenFileName(self, 'Open file',
                                                             baseDirectory, "XML files (*.xml)")
+        # Get filename
         self.filename = os.path.splitext(os.path.basename(self.filepath))[0]
 
+    # Method to export analysis results into SVG file using dialog box
     def export(self):
+        # Get path where to store file
         self.output = Path(str(QFileDialog.getExistingDirectory(
             self, "Select Directory")))
+        # Copy file from internal temp storage to selected storage
         copyfile(str(self.temppath / (self.filename + '.svg')),
                  str(self.output / (self.filename + '.svg')))
 
+    # Method to run analysis
     def runAnalysis(self):
+        # Only proceed if we have a file defined
         if self.filepath != '':
+            # Use TeiParser to read file
             teiParserObject = TeiParser(self.filepath)
 
+            # Get results of vowel calculation considering percentage mode
             vowelCalcObject = VowelCalculator(teiParserObject.parse())
             if (self.cbPercentage.isChecked()):
                 vowelResult = vowelCalcObject.calcpercentage()
             else:
                 vowelResult = vowelCalcObject.calc()
 
+            # Handle saving of results into chart considering percentage mode
             chartExporterObject = ChartExporter(
                 vowelResult[0], vowelResult[1], self.filename, str(self.temppath / (self.filename + '.svg')))
             if (self.cbPercentage.isChecked()):
@@ -81,9 +118,25 @@ class VowelAnalyzerGui(QMainWindow):
             else:
                 chartExporterObject.export()
 
+            # Load saved chart results into web view to display them
             self.wvResult.load(
                 QUrl("file:"+(self.temppath / (self.filename + '.svg')).as_posix()))
 
+            # Set value to indicate that we have performed first analysis
+            self.analysisRun = True
+
+    # Method to handle change of percentage mode
+    def changePercentView(self, event):
+        # If we have performed first analysis, we want to display percentage mode change directly in our window
+        # So we rerun analysis
+        if (self.analysisRun):
+            self.analysisRun(self)
+
+    # Method to show legal notice window
+    def legal(self):
+        self.legalWindow.show()
+
+    # Method to handle resizing of window, so our widgets don't get distorted
     def resizeEvent(self, event):
         self.wvResult.setGeometry(
             120, 10, self.width() - 150, self.height() - 50)
@@ -91,8 +144,16 @@ class VowelAnalyzerGui(QMainWindow):
 
 
 def main():
+    # Start application and load main window
     app = QApplication([])
     window = VowelAnalyzerGui()
+    # Set taskbar icon depending on OS
+    if (platform.system() == 'Windows'):
+        myappid = 'lmu.italianphilology.vowelanalyzer.0'
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+    if (platform.system() == 'Darwin'):
+        app.setWindowIcon(QIcon(window.window_icon))
+    # Show window and execute application
     window.show()
     app.exec()
 
